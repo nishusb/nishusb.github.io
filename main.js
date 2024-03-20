709aaf439cf1bad0c0459fdc4204270385819cd7
aetheric_coding <viniciusbyrne@gmail.com>
2024-03-19 19:10:00 -1000

    more style tweaks

--- main.js
+++ main.js
@@ -158,7 +158,7 @@ createApp({
             selectedAccidental: 0,
             selectedNote: "C",
             selectedChordType: 'major',
-            selectedMode: 'scales',
+            selectedMode: 'chords',
             selectedRomanNumeral: 'i',
         }
     },
